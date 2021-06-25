426be37d4ff1ca1ae949a1a49b1e90ce2d96419b
pcsmomo <dongwoo_333@hanmail.net>
2021-06-26 08:24:14 +1000

    the start of plugin

---
+++ tutorial-plugin/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg)$/,
+        type: 'asset',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024 // 3 kilobytes, The default size is 8kb
+          }
+        }
+      },
+      {
+        test: /\.txt/,
+        type: 'asset/source'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+            // plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  }
+};
