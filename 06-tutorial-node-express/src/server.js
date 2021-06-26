4097b0ba7870790426dddf848de82be974dfba60
pcsmomo <dongwoo_333@hanmail.net>
2021-06-27 07:47:13 +1000

    45. Serving HTML Pages via Express

--- 06-tutorial-node-express/src/server.js
+++ 06-tutorial-node-express/src/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Some dummy content');
+  const pathToHtmlFile = path.resolve(__dirname, '../dist/index.html');
+  const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+  res.send(contentFromHtmlFile);
 });
 
 app.listen(3000, function () {
