422b5c128235914419bcbb32e77494629c4b24e9
pcsmomo <dongwoo_333@hanmail.net>
2021-06-26 15:29:42 +1000

    THE START OF MODE

---
+++ 04-tutorial-mode/src/index.js
@@ -0,0 +1,8 @@
+import Heading from './components/heading/heading.js';
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
+
+const heading = new Heading();
+heading.render();
+
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
